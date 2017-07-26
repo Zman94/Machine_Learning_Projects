21426a1cf07e3775979fa5a8ee7ad73a20aaf5c6
Zachary Gleason <Zachary.Gleason@ibm.com>
2017-07-26 16:07:43 -0400

    Finished basic tutorial

---
+++ house_price/housing_learn.py
@@ -0,0 +1,31 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import numpy as np
+
+# Features
+features = [tf.contrib.layers.real_valued_column("x", dimension=1)]
+
+# Estimator
+estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
+
+# Data
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_train}, y_train,
+                                              batch_size = 4,
+                                              num_epochs = 1000)
+eval_input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_eval}, y_eval,
+                                                   batch_size = 4,
+                                                   num_epochs = 1000)
+
+# Start Estimator
+estimator.fit(input_fn=input_fn, steps=1000)
+
+# Evaluate
+train_loss = estimator.evaluate(input_fn=input_fn)
+eval_loss = estimator.evaluate(input_fn=eval_input_fn)
+print("Train loss: %r"% train_loss)
+print("Eval loss: %r"% eval_loss)
