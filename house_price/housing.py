c0e31cfcd65815f7e4c870990775b0edb4223794
Zachary Gleason <Zachary.Gleason@ibm.com>
2017-07-27 09:54:12 -0400

    Completed uni-variable linregression, housing

--- house_price/housing.py
+++ house_price/housing.py
@@ -3,36 +3,40 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import numpy as np
 
-### Model linear regression y = Wx + b ###
-# x = tf.placeholder(tf.float32, [None, 1])
-x = tf.placeholder(tf.float32)
-# x = tf.reduce_mean(x)
-W = tf.Variable([.3], dtype=tf.float32)
-b = tf.Variable([-.3], dtype=tf.float32)
-# W = tf.Variable(tf.zeros([1,1]))
-# b = tf.Variable(tf.zeros([1]))
-h = W*x +b
-# y_ = tf.placeholder(tf.float32, [None, 1])
-y = tf.placeholder(tf.float32)
+steps = 1000
 
-### Cost function 1/(2m) * sum((y_-y)**2) ###
+### Fake housing prices
+xs = [[j for i in range(1)] for j in range(100)]
+ys = [[2*j for i in range(1)] for j in range(100)]
+
+### Model linear regression y = Wx + b ###
+x = tf.placeholder(tf.float32, [None, 1])
+# x = tf.reduce_mean(x)
+W = tf.Variable(tf.zeros([1,1]))
+b = tf.Variable(tf.zeros([1]))
+h = tf.matmul(x,W) + b
+y = tf.placeholder(tf.float32, [None, 1])
+
+### Cost function 1/(m) * sum((y_-y)**2) ###
 cost = tf.reduce_mean(tf.square(h-y))
 
 ### Training using Gradient Descent ###
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+optimizer = tf.train.GradientDescentOptimizer(0.0003)
 train = optimizer.minimize(cost)
-
-### Training data ###
-x_train = [1, 2, 3, 4]
-y_train = [0, -1, -2, -3]
 
 ### Training Loop ###
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
-for i in range(1000):
-    sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
+
+for i in range(steps):
+    sess.run(train, {x:xs, y:ys})
+    print("After %d iterationi:" % i)
+    print("W: %f" % sess.run(W))
+    print("b: %f" % sess.run(b))
 
 ### Results ###
-curr_W, curr_b, curr_loss = sess.run([W, b, cost], {x:x_train, y:y_train})
+# curr_W = sess.run(W)
+# curr_b = sess.run(b)
+curr_W, curr_b, curr_loss = sess.run([W, b, cost], feed_dict={x:xs, y:ys})
 print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
